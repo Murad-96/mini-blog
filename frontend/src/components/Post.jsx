0e8a07a270997b16bd572a8d856ceedb51c5bed1
Murad-96 <muradmagdiyev@gmail.com>
2025-08-31 14:29:52 +0100

    comments upd

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -12,6 +12,7 @@ export default function Post (props) {
         const postComments = await props.addComment(props.id, commentText)
         // setComments
         setComments(postComments)
+        setCommentText('')
     }
 
     return (
@@ -19,7 +20,7 @@ export default function Post (props) {
             <h2>{props.title}</h2>
             <p>{props.content}</p>
             <button type="submit" onClick={props.postDelete}>Delete</button>   
-            <textarea onChange={e => setCommentText(e.target.value)}/>
+            <textarea value={commentText} onChange={e => setCommentText(e.target.value)}/>
             <button onClick={handleCreateComment}>Leave comment</button>
             {comments.map(com => (<PostComment  key={com._id} id={com._id} comment={com.text} author={com.author} />)) }
         </div>
