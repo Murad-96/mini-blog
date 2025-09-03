928580c6b51ea931ca1f9749a46e0e8092c71859
Murad-96 <muradmagdiyev@gmail.com>
2025-09-03 19:02:23 +0100

    remove default post form

--- frontend/src/components/PostForm.jsx
+++ frontend/src/components/PostForm.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
 
 export default function PostForm (props) {
-    const [postContent, setPostContent] = useState('I really enjoyed biking yesterday!')
-    const [postTitle, setPostTitle] = useState('biking')
+    const [postContent, setPostContent] = useState('')
+    const [postTitle, setPostTitle] = useState('')
 
     return (
         <form onSubmit={(e) => {
@@ -12,14 +12,12 @@ export default function PostForm (props) {
             <label>
             Post title:
             <input type="text"
-                    defaultValue="biking"
                     onChange={e => setPostTitle(e.target.value)}></input>
             </label>
             <label>
             Edit your post:
             <textarea
                 name="postContent"
-                defaultValue="I really enjoyed biking yesterday!"
                 rows={4}
                 cols={40}
                 onChange={e => setPostContent(e.target.value)}
